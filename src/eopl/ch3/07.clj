103d7b72ee57976b1de38ea3e8970c60be8a75a8
smileyan <papayado@gmail.com>
2016-03-09 08:24:25 +1030

    Adding ch3 08

--- src/eopl/ch3/07.clj
+++ src/eopl/ch3/07.clj
@@ -1,3 +1,16 @@
 (add-exp (exp1 exp2)
   (diff-exp (exp1 minus(exp2))))
 
+(mul-exp (exp1 exp2)
+        (let [val1 (value-of exp1 env)
+              val2 (value-of exp2 env)]
+          (let [num1 (expval->num val1)
+                num2 (expval->num val2)]
+            (num-val (* num1 num2)))))
+
+(div-exp (exp1 exp2)
+  (let [val1 (value-of exp1 env)
+        val2 (value-of exp2 env)]
+    (let [num1 (expval->num val1)
+          num2 (expval->num val2)]
+      (num-val (quotient num1 num2)))))
