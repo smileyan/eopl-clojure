fbce160ad3e4c0b31b0b02c9d70ac3828ca55dfa
smileyan <papayado@gmail.com>
2016-03-17 08:16:44 +1030

    Adding structual induction and smaller subproblem principle

--- src/eopl/inductive_sets_of_data.rkt
+++ src/eopl/inductive_sets_of_data.rkt
@@ -79,4 +79,70 @@ Definition 1.1.8 (lambda expression)
 
 1.1.3 Induction
       1. to prove theorems about members of the set and 
-      2. to write programs that manipulate them. 
+      2. to write programs that manipulate them.
+
+Theorem 1.1.1 Let t be a binary tree, as definition 1.1.7. Then t contains
+an odd number of nodes.
+
+___________________________________________________________________________________
+                         Proof by Structual Induction
+ To prove that a proposition IH(s) is true for all structures s, prove the follow-
+ ing:
+    1. IH is true on simple structures (those without substructures).
+    2. If IH is true on the substructures of s, then it is true on s itself.
+___________________________________________________________________________________
+
+1.2 Deriving Recursive Programs
+
+_____________________________________________________________________________
+                  The Smaller-Subproblem Principle
+If we can reduce a problem to a smaller subproblem, we can call the procedure
+that solves the problem to solve the subproblem.
+_____________________________________________________________________________
+
+1.2.1 list-length
+
+> (length '(a b c))
+3
+> (length '((x) ()))
+2
+
+list-length : List -> Int
+usage: (list-length l) = the length of l
+(define list-length
+  (lambda (lst)
+    (if (null? lst)
+      0
+      (+ 1 (list-length (cdr lst))))))
+
+(list-length '(a (b c) d))
+= (+ 1 list-length '((b c) d))
+= (+ 1 (+ 1 list-length '(d)))
+= (+ 1 (+ 1 (+ 1 list-length '())))
+= (+ 1 (+ 1 (+ 1 0)))
+= 3
+
+1.2.2 nth-element
+      > (list-ref '(a b c) 1)
+      b
+
+      nth-element : List * Int -> SchemeVal
+      usage: (nth-element lst n) = the n-th element of lst
+      (define nth-element
+        (lambda (lst n)
+          (if (null? lst)
+            (report-list-too-short n)
+            (if (zero? n)
+              (car lst)
+              (nth-element (cdr lst) (- n 1))))))
+      
+      (define report-list-too-short
+        (lambda (n)
+          (eopl:error 'nth-element
+            "List too short by ~s elements.~%" (+ n 1))))
+      
+        (nth-element '(a b c d e) 3)
+      = (nth-element '(b c d e) 2)
+      = (nth-element '(c d e) 1)
+      = (nth-element '(d e) 0)
+      = d
